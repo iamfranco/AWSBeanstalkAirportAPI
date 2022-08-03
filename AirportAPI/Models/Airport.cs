f2c2d030f7b2ddfa94c4345b1b886176135c7985
iamfranco <kinfaifrancochan@gmail.com>
2022-08-03 10:14:47 +0100

    add Airport class and install DynamoDB AWS nuget packages

---
+++ AirportAPI/Models/Airport.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace AirportAPI.Models;
+
+public class Airport
+{
+    [DynamoDBHashKey("code")]
+    public string? Code { get; set; }
+
+    [DynamoDBProperty("name")]
+    public string? Name { get; set; }
+
+    [DynamoDBProperty("city")]
+    public string? City { get; set; }
+}
